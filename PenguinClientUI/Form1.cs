3d19b7314e33840160eeed06cdd0c3aebe23b447
Ariel Bachar <relrelbachar@gmail.com>
2017-06-12 07:33:36 +0300

    Close connection on form closed

--- PenguinClientUI/Form1.cs
+++ PenguinClientUI/Form1.cs
@@ -1,19 +1,28 @@
-﻿using System.Windows.Forms;
-using PenguinClient;
+﻿using System;
 using System.Drawing;
+using System.Windows.Forms;
+using PenguinClient;
 
 namespace PenguinClientUI
 {
 	public partial class Form1 : Form
 	{
+		private Client client;
+
 		public Form1()
 		{
 			InitializeComponent();
 			LogWriter output = new LogWriter(log);
 			LogWriter error = new LogWriter(log);
 			error.ForeColor = Color.Red;
-			Client client = new Client(output, error);
+			client = new Client(output, error);
 			bool connected = client.Connect("158.69.214.194", 6112, 6115, "nagitest", "test");
+		}
+
+		protected override void OnClosed(EventArgs e)
+		{
+			base.OnClosed(e);
+			client.Dispose();
 		}
 	}
 }
