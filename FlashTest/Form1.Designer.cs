9a3bb5f8cc059190008c1a17beaf5fc9df7b288a
Ariel Bachar <relrelbachar@gmail.com>
2017-06-15 00:28:29 +0300

    Testing

--- FlashTest/Form1.Designer.cs
+++ FlashTest/Form1.Designer.cs
@@ -29,36 +29,60 @@
 		private void InitializeComponent()
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-			this.axShockwaveFlash1 = new AxShockwaveFlashObjects.AxShockwaveFlash();
-			((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).BeginInit();
+			this.axShockwaveFlash = new AxShockwaveFlashObjects.AxShockwaveFlash();
+			this.button = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash)).BeginInit();
 			this.SuspendLayout();
 			// 
-			// axShockwaveFlash1
+			// axShockwaveFlash
 			// 
-			this.axShockwaveFlash1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.axShockwaveFlash1.Enabled = true;
-			this.axShockwaveFlash1.Location = new System.Drawing.Point(0, 0);
-			this.axShockwaveFlash1.Name = "axShockwaveFlash1";
-			this.axShockwaveFlash1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axShockwaveFlash1.OcxState")));
-			this.axShockwaveFlash1.Size = new System.Drawing.Size(284, 261);
-			this.axShockwaveFlash1.TabIndex = 0;
+			this.axShockwaveFlash.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.axShockwaveFlash.Enabled = true;
+			this.axShockwaveFlash.Location = new System.Drawing.Point(0, 0);
+			this.axShockwaveFlash.Name = "axShockwaveFlash";
+			this.axShockwaveFlash.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axShockwaveFlash.OcxState")));
+			this.axShockwaveFlash.Size = new System.Drawing.Size(284, 261);
+			this.axShockwaveFlash.TabIndex = 0;
+			// 
+			// button
+			// 
+			this.button.Location = new System.Drawing.Point(106, 0);
+			this.button.Name = "button";
+			this.button.Size = new System.Drawing.Size(75, 23);
+			this.button.TabIndex = 1;
+			this.button.Text = "Waddle";
+			this.button.UseVisualStyleBackColor = true;
+			this.button.Click += new System.EventHandler(this.button_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(0, 0);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 2;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(284, 261);
-			this.Controls.Add(this.axShockwaveFlash1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button);
+			this.Controls.Add(this.axShockwaveFlash);
 			this.Name = "Form1";
 			this.Text = "Form1";
-			((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
-		private AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash1;
+		private AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash;
+		private System.Windows.Forms.Button button;
+		private System.Windows.Forms.TextBox textBox1;
 	}
 }
 
--- FlashTest/Form1.cs
+++ FlashTest/Form1.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Forms;
+﻿using System.Collections;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
 
 namespace FlashTest
 {
@@ -7,8 +10,83 @@ namespace FlashTest
 		public Form1()
 		{
 			InitializeComponent();
-			axShockwaveFlash1.LoadMovie(0, Application.StartupPath + "\\loader.swf");
-			axShockwaveFlash1.Play();
+			axShockwaveFlash.LoadMovie(0, Application.StartupPath + "\\Load.swf");
+			axShockwaveFlash.Play();
+		}
+
+		private void button_Click(object sender, System.EventArgs e)
+		{
+			//1841
+			Waddle(16, 400, 400);
+		}
+
+		private void Waddle(int id, int x, int y)
+		{
+			SendPacket("s", "u#sp", new object[] { id, x, y }, int.Parse(textBox1.Text));
+		}
+
+		private void SendPacket(string extension, string command, object[] array, int internalRoomId)
+		{
+			CallFunction("sendPacket", "void", extension, command, array, "str", internalRoomId);
+		}
+
+		private string CallFunction(string name, string returnType, params object[] args)
+		{
+			StringBuilder builder = new StringBuilder();
+			XmlWriterSettings settings = new XmlWriterSettings();
+			settings.OmitXmlDeclaration = true;
+			using (XmlWriter writer = XmlWriter.Create(builder, settings))
+			{
+				writer.WriteStartElement("invoke");
+				writer.WriteAttributeString("name", name);
+				writer.WriteAttributeString("returnType", returnType);
+				writer.WriteStartElement("arguments");
+				foreach (object arg in args)
+				{
+					WriteXmlArgument(arg, writer);
+				}
+				writer.WriteFullEndElement();
+				writer.WriteFullEndElement();
+			}
+			string request = builder.ToString();
+			return axShockwaveFlash.CallFunction(request);
+		}
+
+		private void WriteXmlArgument(object arg, XmlWriter writer)
+		{
+			if (arg == null)
+			{
+				writer.WriteStartElement("null");
+				writer.WriteEndElement();
+			}
+			else if (arg is bool)
+			{
+				writer.WriteStartElement((bool)arg ? "true" : "false");
+				writer.WriteEndElement();
+			}
+			else if (arg is string)
+			{
+				writer.WriteElementString("string", (string)arg);
+			}
+			else if (arg is float || arg is double || arg is int || arg is uint)
+			{
+				writer.WriteElementString("number", arg.ToString());
+			}
+			else if (arg.GetType().IsArray)
+			{
+				IEnumerable enumerable = (IEnumerable)arg;
+				writer.WriteStartElement("array");
+				int i = 0;
+				foreach (object obj in enumerable)
+				{
+					writer.WriteStartElement("property");
+					writer.WriteAttributeString("id", i.ToString());
+					WriteXmlArgument(obj, writer);
+					writer.WriteFullEndElement();
+					i++;
+				}
+				writer.WriteFullEndElement();
+			}
 		}
 	}
 }
