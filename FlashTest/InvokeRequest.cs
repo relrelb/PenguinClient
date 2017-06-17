6a5462f86c866be7a6bbaeea1c943f5be199e7da
Ariel Bachar <relrelbachar@gmail.com>
2017-06-17 04:15:58 +0300

    Added Util and InvokeRequest classes

---
+++ FlashTest/InvokeRequest.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+
+namespace FlashTest
+{
+	public class InvokeRequest
+	{
+		#region Fields
+
+		private string name;
+
+		private object[] args;
+
+		#endregion
+
+		#region Properties
+
+		public string Name { get { return name; } }
+
+		public object[] Arguments { get { return args; } }
+
+		#endregion
+
+		#region Constructors
+
+		public InvokeRequest(string name, IEnumerable args)
+		{
+			this.name = name;
+			this.args = args.Cast<object>().ToArray();
+		}
+
+		public InvokeRequest(string name) : this(name, new object[0]) { }
+
+		#endregion
+
+		#region Methods
+
+		public static InvokeRequest Parse(string request)
+		{
+			using (XmlReader reader = XmlReader.Create(new StringReader(request)))
+			{
+				reader.ReadStartElement("invoke");
+				string name = reader.GetAttribute("name");
+				string returnType = reader.GetAttribute("returnType");
+				reader.ReadStartElement("arguments");
+				List<object> args = new List<object>();
+				while (reader.Name != "arguments" || reader.NodeType != XmlNodeType.EndElement)
+				{
+					object arg = Util.ReadXmlValue(reader);
+					args.Add(arg);
+				}
+				reader.ReadEndElement();
+				reader.ReadEndElement();
+				return new InvokeRequest(name, args);
+			}
+		}
+
+		#endregion
+	}
+}
---
+++ FlashTest/Util.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace FlashTest
+{
+	public static class Util
+	{
+		public static string GetString(object obj)
+		{
+			if (obj == null)
+				return "null";
+			Dictionary<string, object> dict = obj as Dictionary<string, object>;
+			if (dict == null)
+				return obj.ToString();
+			string str = "{";
+			bool comma = false;
+			foreach (KeyValuePair<string, object> prop in dict)
+			{
+				if (comma)
+					str += ", ";
+				str += prop.Key + ": " + GetString(prop.Value);
+				comma = true;
+			}
+			str += "}";
+			return str;
+		}
+
+		public static object SendPacket(AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash, string extension, string command, object[] array, int internalRoomId)
+		{
+			for (int i = 0; i < array.Length; i++)
+			{
+				array[i] = array[i].ToString();
+			}
+			return CallFunction(axShockwaveFlash, "sendPacket", extension, command, array, "str", internalRoomId);
+		}
+
+		public static string ReceivePacket(AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		private static object CallFunction(AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash, string name, params object[] args)
+		{
+			StringBuilder builder = new StringBuilder();
+			XmlWriterSettings settings = new XmlWriterSettings();
+			settings.OmitXmlDeclaration = true;
+			using (XmlWriter writer = XmlWriter.Create(builder, settings))
+			{
+				writer.WriteStartElement("invoke");
+				writer.WriteAttributeString("name", name);
+				writer.WriteAttributeString("returnType", "xml");
+				writer.WriteStartElement("arguments");
+				foreach (object arg in args)
+				{
+					WriteXmlValue(arg, writer);
+				}
+				writer.WriteFullEndElement();
+				writer.WriteFullEndElement();
+			}
+			string request = builder.ToString();
+			string result = axShockwaveFlash.CallFunction(request);
+			using (XmlReader reader = XmlReader.Create(new StringReader(result)))
+			{
+				return ReadXmlValue(reader);
+			}
+		}
+
+		internal static void WriteXmlValue(object value, XmlWriter writer)
+		{
+			if (value == null)
+			{
+				writer.WriteStartElement("null");
+				writer.WriteEndElement();
+			}
+			else if (value is bool)
+			{
+				writer.WriteStartElement((bool)value ? "true" : "false");
+				writer.WriteEndElement();
+			}
+			else if (value is string)
+			{
+				writer.WriteElementString("string", (string)value);
+			}
+			else if (value is float || value is double || value is int || value is uint)
+			{
+				writer.WriteElementString("number", value.ToString());
+			}
+			else if (value.GetType().IsArray)
+			{
+				IEnumerable enumerable = (IEnumerable)value;
+				writer.WriteStartElement("array");
+				int i = 0;
+				foreach (object item in enumerable)
+				{
+					writer.WriteStartElement("property");
+					writer.WriteAttributeString("id", i.ToString());
+					WriteXmlValue(item, writer);
+					writer.WriteFullEndElement();
+					i++;
+				}
+				writer.WriteFullEndElement();
+			}
+		}
+
+		internal static object ReadXmlValue(XmlReader reader)
+		{
+			reader.Read();
+			switch (reader.Name)
+			{
+				case "null":
+				case "undefined":
+					reader.Read();
+					return null;
+				case "true":
+					reader.Read();
+					return true;
+				case "false":
+					reader.Read();
+					return false;
+				case "string":
+					return reader.ReadElementContentAsString();
+				case "number":
+					string number = reader.ReadElementContentAsString();
+					int i;
+					if (int.TryParse(number, out i))
+						return i;
+					float f;
+					if (float.TryParse(number, out f))
+						return f;
+					double d;
+					if (double.TryParse(number, out d))
+						return d;
+					throw new InvalidDataException("Invalid number");
+				case "array":
+					List<object> list = new List<object>();
+					if (reader.ReadToDescendant("property"))
+					{
+						do
+						{
+							reader.MoveToAttribute("id");
+							int id = reader.ReadContentAsInt();
+							object value = ReadXmlValue(reader);
+							list.Add(value);
+						} while (reader.ReadToNextSibling("property"));
+					}
+					reader.ReadEndElement();
+					return list.ToArray();
+				case "object":
+					Dictionary<string, object> obj = new Dictionary<string, object>();
+					if (reader.ReadToDescendant("property"))
+					{
+						do
+						{
+							string key = reader.GetAttribute("id");
+							object value = ReadXmlValue(reader);
+							obj.Add(key, value);
+						} while (reader.ReadToNextSibling("property"));
+					}
+					reader.ReadEndElement();
+					return obj;
+			}
+			throw new InvalidDataException("Invalid XML data");
+		}
+	}
+}
