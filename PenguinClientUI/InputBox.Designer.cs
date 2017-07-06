25527ba0a9f2ac3dc32a6c5559e9bcc8d6e5d6f7
Ariel Bachar <relrelbachar@gmail.com>
2017-07-06 20:26:51 +0300

    Added InputBox

---
+++ PenguinClientUI/InputBox.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace PenguinClientUI
+{
+	partial class InputBox
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.cancel = new System.Windows.Forms.Button();
+			this.ok = new System.Windows.Forms.Button();
+			this.input = new System.Windows.Forms.TextBox();
+			this.text = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// cancel
+			// 
+			this.cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.cancel.Location = new System.Drawing.Point(116, 59);
+			this.cancel.Name = "cancel";
+			this.cancel.Size = new System.Drawing.Size(75, 23);
+			this.cancel.TabIndex = 2;
+			this.cancel.Text = "&Cancel";
+			this.cancel.UseVisualStyleBackColor = true;
+			// 
+			// ok
+			// 
+			this.ok.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.ok.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.ok.Location = new System.Drawing.Point(197, 59);
+			this.ok.Name = "ok";
+			this.ok.Size = new System.Drawing.Size(75, 23);
+			this.ok.TabIndex = 3;
+			this.ok.Text = "&OK";
+			this.ok.UseVisualStyleBackColor = true;
+			// 
+			// input
+			// 
+			this.input.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+			| System.Windows.Forms.AnchorStyles.Right)));
+			this.input.Location = new System.Drawing.Point(12, 29);
+			this.input.Name = "input";
+			this.input.Size = new System.Drawing.Size(260, 20);
+			this.input.TabIndex = 1;
+			// 
+			// text
+			// 
+			this.text.AutoSize = true;
+			this.text.Location = new System.Drawing.Point(13, 13);
+			this.text.Name = "text";
+			this.text.Size = new System.Drawing.Size(0, 13);
+			this.text.TabIndex = 0;
+			// 
+			// InputBox
+			// 
+			this.AcceptButton = this.ok;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.cancel;
+			this.ClientSize = new System.Drawing.Size(284, 94);
+			this.Controls.Add(this.text);
+			this.Controls.Add(this.input);
+			this.Controls.Add(this.ok);
+			this.Controls.Add(this.cancel);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "InputBox";
+			this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.ShowInTaskbar = false;
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button cancel;
+		private System.Windows.Forms.Button ok;
+		private System.Windows.Forms.TextBox input;
+		private System.Windows.Forms.Label text;
+	}
+}
---
+++ PenguinClientUI/InputBox.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PenguinClientUI
+{
+	public partial class InputBox : Form
+	{
+		#region Constructors
+
+		private InputBox()
+		{
+			InitializeComponent();
+		}
+
+		private InputBox(string text) : this()
+		{
+			this.text.Text = text;
+		}
+
+		private InputBox(string text, string caption) : this(text)
+		{
+			Text = caption;
+		}
+
+		#endregion
+
+		#region Methods
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+			ActiveControl = input;
+		}
+
+		private static string Show(InputBox box)
+		{
+			DialogResult result = box.ShowDialog();
+			if (result == DialogResult.OK)
+				return box.input.Text;
+			return null;
+		}
+
+		public static new string Show()
+		{
+			return Show(new InputBox());
+		}
+
+		public static string Show(string text)
+		{
+			return Show(new InputBox(text));
+		}
+
+		public static string Show(string text, string caption)
+		{
+			return Show(new InputBox(text, caption));
+		}
+
+		private static string Show(InputBox box, IWin32Window owner)
+		{
+			DialogResult result = box.ShowDialog(owner);
+			if (result == DialogResult.OK)
+				return box.input.Text;
+			return null;
+		}
+
+		public static new string Show(IWin32Window owner)
+		{
+			return Show(new InputBox(), owner);
+		}
+
+		public static string Show(IWin32Window owner, string text)
+		{
+			return Show(new InputBox(text), owner);
+		}
+
+		public static string Show(IWin32Window owner, string text, string caption)
+		{
+			return Show(new InputBox(text, caption), owner);
+		}
+
+		#endregion
+	}
+}
